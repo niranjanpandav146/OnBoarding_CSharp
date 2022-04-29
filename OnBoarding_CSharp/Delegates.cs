ca1ffea5f70c5146e9cf63c2e1645f54186fb186
niranjanpandav1469 <niranjan.146@gmail.com>
2022-04-29 09:45:45 +0530

    Added LINQ and Delegates Programs

---
+++ OnBoarding_CSharp/Delegates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoarding_CSharp
+{
+    public delegate void MyDelegate(string msg);
+
+    public class Delegates
+    {
+        
+        
+
+        static void Main(string[] args)
+        {
+            //Implementation of defined delegate
+            MyDelegate del = ClassA.MethodA;
+            del("Called MethodA of ClassA using Delegate");
+
+            // Implementation of normal function delegate
+            Func<int, int, int> add = ClassA.Sum;
+
+            int result = add(10, 20);
+            Console.WriteLine("Functional Delegate Returned Value"+result);
+        }
+    }
+
+    public class ClassA
+    {
+        public static void MethodA(string message)
+        {
+            Console.WriteLine(message);            
+        }
+
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ OnBoarding_CSharp/LINQ.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoarding_CSharp
+{
+    public class LINQ
+    {
+        //public static void Main(string [] args)
+        //{
+        //    // Student collection
+        //    IList<Student> studentList = new List<Student>() {
+        //    new Student() { StudentID = 1, StudentName = "Anand", Age = 18, StandardID = 1 } ,
+        //        new Student() { StudentID = 2, StudentName = "Kishan",  Age = 21, StandardID = 1 } ,
+        //        new Student() { StudentID = 3, StudentName = "Krishna",  Age = 18, StandardID = 2 } ,
+        //        new Student() { StudentID = 4, StudentName = "Ram" , Age = 20, StandardID = 2 } ,
+        //        new Student() { StudentID = 5, StudentName = "Prashant" , Age = 21 }
+        //};
+
+        //    var studentNames = studentList.Where(s => s.Age > 18)
+        //                          .Select(s => s)
+        //                          .Where(st => st.StandardID > 0)
+        //                          .Select(s => s.StudentName);
+
+
+        //    foreach (var name in studentNames)
+        //    {
+        //        Console.WriteLine(name);
+        //    }
+
+        //}
+
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public int StandardID { get; set; }
+    }
+}
